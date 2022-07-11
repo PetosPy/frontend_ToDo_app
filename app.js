6a1b6c14c8d3de2ba6dd47c844ecfd82051eadca
Michael Haraeb <michael.haraeb@aquare.la>
2022-07-10 22:24:39 -0300

    feature: added a Work-todo page to and made some css styling changes

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var items = [];
+let items = [];
+let workTodos = [];
+
+let today = new Date();
 
 app.get("/", (req, res) => {
   var options = {
@@ -21,11 +24,10 @@ app.get("/", (req, res) => {
     day: "numeric",
   };
 
-  let today = new Date();
   let todaysDate = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    todaysDate: todaysDate,
+    listHeadings: todaysDate,
     newListItems: items,
     year: today.getFullYear(),
   });
@@ -33,11 +35,28 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   const newData = req.body.newItem;
+  let itemType = req.body.list;
+
   if (newData.length != 0 && newData.trim() != "" && items.length < 5) {
-    items.push(newData);
-    console.log(newData);
-    res.redirect("/");
+    if (itemType === "Work List") {
+      workTodos.push(newData);
+
+      res.redirect("/work");
+    } else {
+      items.push(newData);
+
+      res.redirect("/");
+    }
   }
+});
+
+app.get("/work", function (req, res) {
+  // console.log(req.body.list);
+  res.render("work", {
+    listHeadings: "Work List",
+    newListItems: workTodos,
+    year: today.getFullYear(),
+  });
 });
 
 app.listen(3000, () => {
